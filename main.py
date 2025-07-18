28b2caf6291a4e0a75e2541c54f43cf09cef5813
Durjoy <swe.eng.durjoy15@gmail.com>
2025-07-18 17:56:06 +0600

    path parameter

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI  # type: ignore
+from fastapi import FastAPI, Path  # type: ignore
 import json
 app = FastAPI()
 
@@ -23,3 +23,10 @@ def view():
     data = load_data()
     return data
     
+@app.get('/patient/{patient_id}')
+def view_patient(patient_id: str):
+    data = load_data()
+
+    if patient_id in data:
+        return data[patient_id]
+    return {'error': 'Patient not found'}
