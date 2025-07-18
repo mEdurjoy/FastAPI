3f39a5864c3d89c72e4245a651920a824bee529b
Durjoy <swe.eng.durjoy15@gmail.com>
2025-07-18 13:55:50 +0600

    Here I read json file

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
-from fastapi import FastAPI 
-
+from fastapi import FastAPI  # type: ignore
+import json
 app = FastAPI()
+
+def load_data():
+    with open('patients.json', 'r')as f:
+        data = json.load(f)
+
+        return data
+
 
 @app.get("/")
 def hello():
-    return {'message': 'Hello World'}
+    return {'message': '{Patient Management System API}'}
 
 @app.get('/about')
 def about():
-    return {'message': "This is durjoy"}
+    return {'message': "A fuly funcational API to manage your patient records"}
+
+
+@app.get('/view')
+def view():
+    data = load_data()
+    return data
+    
